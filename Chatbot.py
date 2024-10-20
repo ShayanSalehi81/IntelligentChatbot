4830ed8f2d885097768a8db3001bf77c929d7bb7
Shayan Salehi <79784127+ShayanSalehi81@users.noreply.github.com>
2024-10-20 13:43:42 +0330

    Add ChatBotCore

--- Chatbot.py
+++ Chatbot.py
@@ -1,16 +1,19 @@
 from SpellingMistakeCorrector.SpellingMistakesCorrector import SpellingMistakeCorrector
 from SwearWordDetector.SwearWordDetector import SwearWordDetector
+from QuestionAnswerer.ChatBotCore import ChatBot
 
 
 class Chatbot:
     def __init__(self) -> None:
         self.swear_detector = SwearWordDetector('SwearWordDetector/swear_words.json')
         self.spelling_corrector = SpellingMistakeCorrector('SpellingMistakeCorrector/frequency_combined.csv', threshold=0.2)
+        self.chat_bot = ChatBot(questions_data_file_path='Datasets\Result_QA.csv')
 
     def chat(self, query) -> str:
-        spellCorrectedQuery = self.spelling_corrector.correct_spelling(query)
-        censoredQuery = self.swear_detector.filter_words(spellCorrectedQuery)
-        return censoredQuery
+        spell_corrected_query = self.spelling_corrector.correct_spelling(query)
+        censored_query = self.swear_detector.filter_words(spell_corrected_query)
+        most_similar_question = self.chat_bot.find_most_similar_question(query=censored_query)
+        return most_similar_question
 
 
 if __name__ == '__main__':
---
+++ QuestionAnswerer/ChatBotCore.py
@@ -0,0 +1,60 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModel
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class ChatBot:
+    def __init__(self, questions_data_file_path):
+        self.qa_dataframe = self.load_data(questions_data_file_path)
+        self.target_questions = self.qa_dataframe['target_question'].to_list()
+        self.tokenizer = AutoTokenizer.from_pretrained("sharif-dal/dal-bert")
+        self.embedding_model = AutoModel.from_pretrained("sharif-dal/dal-bert")
+        self.dataset_embeddings = self.get_or_generate_embeddings(df=self.qa_dataframe, file_path='train_df1_embeddings.npy')
+
+    def load_data(self, file_path):
+        return pd.read_csv(file_path)
+    
+    def get_one_sentence_embedding(self, sentence):
+        inputs = self.tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
+        with torch.no_grad():
+            output = self.embedding_model(**inputs)
+        return output
+    
+    def get_embeddings(self, sentences):
+        embeddings = []
+        for sentence in tqdm(sentences, desc="Generating embeddings"):
+            inputs = self.tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
+            with torch.no_grad():
+                outputs = self.embedding_model(**inputs)
+            embeddings.append(outputs.last_hidden_state.mean(dim=1).numpy())
+        return np.vstack(embeddings)
+    
+    def save_embeddings(self, embeddings, file_path):
+        np.save(file_path, embeddings)
+
+    def load_embeddings(self, file_path):
+        return np.load(file_path)
+    
+    def get_or_generate_embeddings(self, df, file_path):
+        if os.path.exists(file_path):
+            print(f"Loading embeddings from {file_path}")
+            return self.load_embeddings(file_path)
+        else:
+            print(f"Generating embeddings for {file_path}")
+            embeddings = self.get_embeddings(df.tolist())
+            self.save_embeddings(embeddings, file_path)
+            return embeddings
+        
+    def calculate_cosine_similarity(self, embeddings1, embeddings2):
+        return cosine_similarity(embeddings1, embeddings2)
+    
+    def find_most_similar_question(self, query):
+        query_embedding = self.get_one_sentence_embedding(query)
+        similarity_matrix = self.calculate_cosine_similarity(query_embedding, self.dataset_embeddings)
+        predicted_indices = similarity_matrix.argmax(axis=1)
+        return self.target_questions[predicted_indices]
